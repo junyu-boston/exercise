16c46a47f476f9c2be191c093d6c789a82ea796d
Jun Yu <24700686+junyu-boston@users.noreply.github.com>
2021-05-20 12:31:19 -0400

    Update rnn.py

--- rnn.py
+++ rnn.py
@@ -10,3 +10,20 @@ def get_next_char(model, initial_text, chars_window, char_to_index, index_to_cha
 # Define context sentence and print the generated text
 initial_text = "I am not insane, "
 print("Next character: {0}".format(get_next_char(model, initial_text, 20, char_to_index, index_to_char)))
+
+
+# Instantiate the vectors
+sentences = []
+next_chars = []
+# Loop for every sentence
+for sentence in sheldon.split('\n'):
+    # Get 20 previous chars and next char; then shift by step
+    for i in range(0, len(sentence) - chars_window, step):
+        sentences.append(sentence[i:i + chars_window])
+        next_chars.append(sentence[i + chars_window])
+
+# Define a Data Frame with the vectors
+df = pd.DataFrame({'sentence': sentences, 'next_char': next_chars})
+
+# Print the initial rows
+print(df.head())
